1099e60c39afcc4f7774fdafe0d750030178bb44
unknown <christine.mitsch@stud.fh-luebeck.de>
2020-06-09 16:40:57 +0200

    :tada: Initial commit

---
+++ src/firebase/init.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+// The configuration below is not sensitive data.
+const config = {
+  apiKey: 'AIzaSyDFJnDUOQUIgR1tbpxU_gsnWS2F2vWN39c',
+  authDomain: 'weebo-b37d8.firebaseapp.com',
+  databaseURL: 'https://weebo-b37d8.firebaseio.com',
+  projectId: 'weebo-b37d8',
+  storageBucket: 'weebo-b37d8.appspot.com',
+  messagingSenderId: '691998187476',
+  appId: '1:691998187476:web:b4389079f0b2469b9ac041',
+  measurementId: 'G-GCGZK5SJQ5'
+}
+
+firebase.initializeApp(config)
