96a540d7c7040c2baa80a4fba876b952bb581cdc
Chrissy <mitsch.chrissy@gmail.com>
2020-06-22 01:43:51 +0200

    prepared discussion section

--- src/views/Projects/ProjectDiscussion.vue
+++ src/views/Projects/ProjectDiscussion.vue
@@ -3,23 +3,30 @@
         <md-chip>{{ currentProject.name }} / Diskussion</md-chip>
         <div class="md-layout md-gutter">
             <div class="md-layout-item">
-                <md-card class="discussion">
+                <p v-if="!finishedLoading" class="text-center">
+                    <md-progress-spinner class="md-accent" :md-diameter="30" :md-stroke="3" md-mode="indeterminate"></md-progress-spinner><br />
+                    Aktivitäten werden geladen...
+                </p>
+
+                <md-card class="discussion" v-if="finishedLoading">
                     <p class="md-caption text-center">21.06.2020</p>
-                    <md-divider></md-divider>
-                    <md-card-header>
-                        <span class="md-body-2">Hans Wurst</span>
-                        <span class="md-caption"> 19:15 Uhr</span>
-                    </md-card-header>
-                    <md-card-content>
-                        Hallo Leute
-                    </md-card-content>
+                    <div v-for="(message) in messageList" :key="message.id">
+                        <md-divider></md-divider>
+                        <md-card-header>
+                            <span class="md-body-2">Hans Wurst</span>
+                            <span class="md-caption"> 19:15 Uhr</span>
+                        </md-card-header>
+                        <md-card-content>
+                            {{message.data.text}}
+                        </md-card-content>
 
-                    <md-divider></md-divider>
+                        <md-divider></md-divider>
 
-                    <md-card-header class="md-body-2">Hans Wurst</md-card-header>
-                    <md-card-content>
-                        Blabla hat was hochgeladen
-                    </md-card-content>
+                        <md-card-header class="md-body-2">Hans Wurst</md-card-header>
+                        <md-card-content>
+                            Blabla hat was hochgeladen
+                        </md-card-content>
+                    </div>
                 </md-card>
             </div>
         </div>
@@ -32,12 +39,35 @@
     export default {
         computed: {
             ...mapState('app', ['networkOnLine']),
-            ...mapState('projects', ['currentProject'])
+            ...mapState('projects', ['currentProject']),
+            ...mapState('messages', ['messages'])
         },
         data: () => ({
-            finishedLoading: 0
+            finishedLoading: 0,
+            messageList: []
         }),
+        methods: {
+            sortMessageList(list) {
+                function compare(a, b) {
+                    if (a.createTimestamp < b.createTimestamp)
+                        return -1;
+                    if (a.createTimestamp > b.createTimestamp)
+                        return 1;
+                    return 0;
+                }
+                return list.sort(compare);
+            },
+        },
         created() {
+            if (this.currentProject) {
+                this.$store.dispatch('messages/getMessages', this.currentProject.id).then(() => {
+                    const messageToSort = JSON.parse(JSON.stringify(this.messages));
+                    this.messageList = this.sortMessageList(messageToSort);
+                    console.log(this.messageList)
+                }).finally(() => {
+                    this.finishedLoading = true;
+                });
+            }
         }
     }
 </script>
