92d75548f5cee157a0da453f86bc922fc081f936
Chrissy <mitsch.chrissy@gmail.com>
2020-07-05 20:14:57 +0200

    phase descriptions

--- src/views/Projects/ProjectPhase2.vue
+++ src/views/Projects/ProjectPhase2.vue
@@ -1,60 +1,60 @@
 <template>
     <div class="ProjectPhase2">
-        <Modal :showModal="rewardModalActive" @closeModal="rewardModalActive=false" size="large">
-            <md-progress-spinner v-if="!tutorialSaved" class="md-accent" :md-stroke="3" md-mode="indeterminate"></md-progress-spinner>
-            <div v-if="tutorialSaved">
-                <div class="md-display-1">Projektphase 2!</div>
-                <p class="description"><img src="@/assets/img/rakete.png" width="200" /></p>
-                <p class="description md-body-2">In der zweiten Phase deines Projekts geht es um die Spezifikation des Nutzungskontexts.</p>
-                <p class="description md-body-2">Dokumentiere und visualisiere die Analyseergebnisse.</p>
-                <p class="description md-body-2">Erstelle Personas und Szenarien mithilfe von <i>Weebo</i>-Vorlagen.</p>
-                <p class="description md-body-2">Diskutiere mit allen Teilnehmern über die Ergebnisse.</p>
-            </div>
-        </Modal>
+        <tutorial-modal tutorialName="Phase1">
+            <div class="md-display-1 text-center">Projektphase 2!</div>
+            <p class="description"><img src="@/assets/img/rakete.png" width="200" /></p>
+            <p class="description md-body-2">In der zweiten Phase deines Projekts geht es um die Spezifikation der Anforderungen.</p>
+            <p class="description md-caption">Dokumentiere und visualisiere die Analyseergebnisse (Anforderungen).</p>
+            <p class="description md-caption">Diskutiere mit allen Projektteilnehmern über Personas und Dateien.</p>
+            <p class="description md-caption">Diskutiere die Organisation des Projekts und sorge für Transparenz.</p>
+        </tutorial-modal>
 
         <md-chip>{{ currentProject.name }} / Prozess / 2. Spezifikation</md-chip>
+        <md-list class="files-list">
+            <md-list-item md-expand>
+                <span class="md-list-item-text">Dateien</span>
+
+                <div slot="md-expand">
+                </div>
+            </md-list-item>
+        </md-list>
+
+        <p class="text-center"><img src="@/assets/img/phase2.png" class="phase-image" /></p>
+        <md-card>
+            <md-card-header class="md-title">Nutzungsanforderungen festlegen</md-card-header>
+            <md-card-content>
+                Bei den meisten Gestaltungsprojekten stellt die Bestimmung der Benutzererfordernisse und die Festlegung der funktionalen und weiteren Anforderungen an das Produkt oder System eine Hauptaktivität dar. Bei der menschzentrierten Gestaltung muss diese Aktivität erweitert werden, um eine klare Darstellung der Nutzungsanforderungen im Zusammenhang mit der Beschreibung des vorgesehenen Nutzungskontexts und der wirtschaftlichen Ziele des Systems zu liefern.
+            </md-card-content>
+            <md-card-content>
+                In Abhängigkeit vom Anwendungsbereich des Systems können die Nutzungsanforderungen Forderungen nach organisatorischen Änderungen und abgeänderten Arbeitsweisen umfassen, und sie können Vorschläge in Bezug auf Möglichkeiten zur Kombination von Produkten und Dienstleistungen unterbreiten. Wenn bekannt ist, dass das vorgeschlagene interaktive System die organisatorische Praxis beeinflussen wird, sollten organisatorische Stakeholder in den Entwicklungsprozess einbezogen werden, um sowohl die organisatorischen als auch die technischen Systeme zu optimieren.
+            </md-card-content>
+            <md-card-actions><p class="md-caption">DIN EN ISO 9241-210</p></md-card-actions>
+        </md-card>
     </div>
 </template>
 
 <script>
     import {mapState} from "vuex";
-    import Modal from "../../components/Modal";
+    import TutorialModal from "../../components/rewards/TutorialModal";
 
     export default {
         computed: {
             ...mapState('rewards', ['tutorials']),
             ...mapState('projects', ['currentProject'])
         },
-        components: {Modal},
-        data: () => ({
-            tutorialSaved: false,
-            rewardModalActive: false
-        }),
-        created() {
-            setTimeout(function () { this.triggerReward() }.bind(this), 1000);
-        },
-        methods: {
-            triggerReward() {
-                const checkIfUserHasTutorialFinished = this.tutorials.filter(function(elem) {
-                    if(elem.name === "Phase2") return elem;
-                    return null;
-                });
-                if (checkIfUserHasTutorialFinished.length > 0) {
-                    this.rewardModalActive = false;
-                } else {
-                    this.rewardModalActive = true;
-                    const tutorial = {
-                        name: "Phase2",
-                        type: "tutorial"
-                    };
-                    this.$store.dispatch('rewards/createFinishedTutorial', tutorial).then(() => {
-                        this.tutorialSaved = true;
-                    });
-                }
-            }
-        }
+        components: {TutorialModal},
     };
 </script>
 
 <style lang="scss" scoped>
+    .files-list {
+        margin-top: 24px;
+    }
+
+    .phase-image {
+        margin-top: 24px;
+        margin-bottom: 24px;
+        width: 100%;
+        max-width: 500px;
+    }
 </style>
--- src/views/Projects/ProjectPhase4.vue
+++ src/views/Projects/ProjectPhase4.vue
@@ -1,58 +1,74 @@
 <template>
     <div class="ProjectPhase2">
-        <Modal :showModal="rewardModalActive" @closeModal="rewardModalActive=false" size="large">
-            <md-progress-spinner v-if="!tutorialSaved" class="md-accent" :md-stroke="3" md-mode="indeterminate"></md-progress-spinner>
-            <div v-if="tutorialSaved">
-                <div class="md-display-1">Letzte Phase!</div>
-                <p class="description"><img src="@/assets/img/rakete.png" width="200" /></p>
-                <p class="description md-body-2">Feedback der Nutzer ist nicht nur in den vorherigen Phasen wichtig!</p>
-                <p class="description md-body-2">Plane und dokumentiere Nutzertests.</p>
-            </div>
-        </Modal>
+        <tutorial-modal tutorialName="Phase3">
+            <div class="md-display-1 text-center">Letzte Phase!</div>
+            <p class="description"><img src="@/assets/img/rakete.png" width="200" /></p>
+            <p class="description md-body-2">
+                Feedback der Nutzer ist nicht nur in den vorherigen Phasen wichtig!
+                Insbesondere in der Evaluationsphase entscheidet das Nutzerfeedback über den weiteren Verlauf des
+                nutzerzentrierten Designprozesses.
+            </p>
+            <p class="description md-caption">Plane Termine für User-Tests und andere nutzerzentrierte Evaluationsmethoden.</p>
+        </tutorial-modal>
 
         <md-chip>{{ currentProject.name }} / Prozess / 4. Evaluation</md-chip>
+        <md-list class="files-list">
+            <md-list-item md-expand>
+                <span class="md-list-item-text">Dateien</span>
+
+                <div slot="md-expand">
+                </div>
+            </md-list-item>
+        </md-list>
+
+        <p class="text-center"><img src="@/assets/img/phase4.png" class="phase-image" /></p>
+        <md-card>
+            <md-card-header class="md-title">Design evaluieren</md-card-header>
+            <md-card-content>
+                Benutzerzentrierte Evaluierung (die Evaluierung aus der Benutzerperspektive) ist innerhalb der mensch-zentrierten Gestaltung eine erforderliche Aktivität.
+            </md-card-content>
+            <md-card-content>
+                Selbst in den frühen Phasen des Projekts sollten Gestaltungskonzepte bewertet werden, um ein besseres Verständnis für die Benutzererfordernisse zu gewinnen. Die reale Nutzung eines Produkts, Systems oder einer Dienstleistung ist komplex und obwohl Leitlinien zur ergonomischen Gestaltung eine nützliche Hilfe für die Gestalter darstellen können, ist die benutzerzentrierte Evaluierung ein entscheidendes Element der menschzentrierten Gestaltung. Dennoch ist die Evaluierung durch Benutzer [...] in jeder Phase des Projekts nicht immer praktikabel oder kostengünstig. Unter diesen Umständen sollten Gestaltungslösungen auch auf andere Weise bewertet werden — zum Beispiel durch die Modellierung von Aufgaben und Simulationen. Diese Methoden konzentrieren sich auf die Frage, wie Benutzer das System erfahren, selbst wenn die Benutzer möglicherweise nicht direkt beteiligt sind. Die benutzerzentrierte Evaluierung kann verwendet werden, um:
+            </md-card-content>
+            <md-card-content>
+                a) neue Informationen über die Benutzererfordernisse zu sammeln;
+            </md-card-content>
+            <md-card-content>
+                b) Rückmeldungen über die Stärken und Schwächen der Gestaltungslösung aus der Benutzerperspektive zu geben (um die Gestaltung zu verbessern);
+            </md-card-content>
+            <md-card-content>
+                c) zu beurteilen, ob Nutzungsanforderungen erreicht wurden (darin kann die Beurteilung der Konformität mit internationalen, nationalen, lokalen, firmeneigenen oder gesetzlichen Normen enthalten sein);
+            </md-card-content>
+            <md-card-content>
+                d) Ausgangsdaten zu erheben oder Vergleiche zwischen Gestaltungsalternativen durchzuführen.
+            </md-card-content>
+            <md-card-actions><p class="md-caption">DIN EN ISO 9241-210</p></md-card-actions>
+        </md-card>
     </div>
 </template>
 
 <script>
     import {mapState} from "vuex";
-    import Modal from "../../components/Modal";
+    import TutorialModal from "../../components/rewards/TutorialModal";
 
     export default {
         computed: {
             ...mapState('rewards', ['tutorials']),
             ...mapState('projects', ['currentProject'])
         },
-        components: {Modal},
-        data: () => ({
-            tutorialSaved: false,
-            rewardModalActive: false
-        }),
-        created() {
-            setTimeout(function () { this.triggerReward() }.bind(this), 1000);
-        },
-        methods: {
-            triggerReward() {
-                const checkIfUserHasTutorialFinished = this.tutorials.filter(function(elem) {
-                    if(elem.name === "Phase4") return elem;
-                    return null;
-                });
-                if (checkIfUserHasTutorialFinished.length > 0) {
-                    this.rewardModalActive = false;
-                } else {
-                    this.rewardModalActive = true;
-                    const tutorial = {
-                        name: "Phase4",
-                        type: "tutorial"
-                    };
-                    this.$store.dispatch('rewards/createFinishedTutorial', tutorial).then(() => {
-                        this.tutorialSaved = true;
-                    });
-                }
-            }
-        }
+        components: {TutorialModal},
     };
 </script>
 
 <style lang="scss" scoped>
+    .files-list {
+        margin-top: 24px;
+    }
+
+    .phase-image {
+        margin-top: 24px;
+        margin-bottom: 24px;
+        width: 100%;
+        max-width: 500px;
+    }
 </style>
